85c0f8a7384c650176403935ae2f43b62180db39
flavio1227 <flavio12.27orellana@gmail.com>
2026-01-09 11:30:50 -0600

    Agregar botÃ³n discreto SIGEM en esquina superior izquierda

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-custom-blue text-custom-yellow py-1">
+    <header className="bg-custom-blue text-custom-yellow py-1 relative">
       <div className="container mx-auto px-4">
         <div className="flex flex-col items-center justify-center">
           {/* Logo con tamaño w-64 h-64 */}
@@ -15,6 +15,16 @@ const Header: React.FC = () => {
           </div>
         </div>
       </div>
+      {/* Botón discreto en la esquina superior izquierda */}
+      <a
+        href="https://flavio1227.github.io/SIGEM1.1/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="absolute top-2 left-2 text-custom-yellow text-xs opacity-50 hover:opacity-100 transition-opacity duration-200 hover:underline"
+        title="Ir a SIGEM"
+      >
+        SIGEM
+      </a>
     </header>
   );
 }
