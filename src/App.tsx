98072ef6a576311803f5aa9007abd2f5489222e4
flavio1227 <flavio12.27orellana@gmail.com>
2026-01-12 14:46:01 -0600

    Remover protecciÃ³n de autenticaciÃ³n - la calculadora funciona como micro app dentro de SIGEM1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import MiningCalculator from './components/MiningCalculator';
-import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <ProtectedRoute>
-      <div className="min-h-screen bg-custom-blue">
-        <Header />
-        <main className="container mx-auto px-4 py-8">
-          <MiningCalculator />
-        </main>
-      </div>
-    </ProtectedRoute>
+    <div className="min-h-screen bg-custom-blue">
+      <Header />
+      <main className="container mx-auto px-4 py-8">
+        <MiningCalculator />
+      </main>
+    </div>
   );
 }
 
